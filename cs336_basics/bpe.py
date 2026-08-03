f8289a3871feb9ce20a33f43a47eb6bda25fc810
sgu-101 <112212519+sgu-101@users.noreply.github.com>
2026-08-03 16:15:05 -0400

    tokenizer attempt1

--- cs336_basics/bpe.py
+++ cs336_basics/bpe.py
@@ -1,12 +1,13 @@
 import regex as re
 import multiprocessing as mp
-from collections import defaultdict 
+from collections import defaultdict
 from cs336_basics.pretokenization_example import find_chunk_boundaries
 
+
 def _get_pretokenized_vocab(
-    input_path: str, 
-    start: int, 
-    end: int, 
+    input_path: str,
+    start: int,
+    end: int,
     special_tokens: list[str]
 ) -> dict[tuple[bytes, ...], int]:
     """
@@ -31,8 +32,9 @@ def _get_pretokenized_vocab(
             bytematch = currstr.encode("utf-8")
             match = tuple(bytematch[i:i+1] for i in range(len(bytematch)))
             pretokenized_vocab[match] = pretokenized_vocab.get(match, 0) + 1
-            
+
     return pretokenized_vocab
+
 
 def _counts_and_vocabmap(
     pretokenized_vocab: dict[tuple[bytes, ...], int]
@@ -40,14 +42,15 @@ def _counts_and_vocabmap(
     """Builds the initial paircounts and reverse lookup vocabmap. The paircounts is the count/frequency of each pair in the entire corpus and the vocabmap is a map from each different pair to all words that contain it"""
     paircounts = {}
     vocabmap = defaultdict(set)
-    
+
     for byteword, count in pretokenized_vocab.items():
         for i in range(len(byteword) - 1):
             pair = (byteword[i], byteword[i + 1])
             paircounts[pair] = paircounts.get(pair, 0) + count
             vocabmap[pair].add(byteword)
-            
+
     return paircounts, vocabmap
+
 
 def _apply_merge(
     bestpair: tuple[bytes, bytes],
@@ -59,17 +62,17 @@ def _apply_merge(
     Updates counts and structures in-place based on the chosen merge (given by bestpair).
     """
 
-    deepcopy = set(vocabmap[bestpair])
-    
-    for byteword in deepcopy:
+    shallowcopy = set(vocabmap[bestpair])
+
+    for byteword in shallowcopy:
         currcount = pretokenized_vocab[byteword]
-        
+
         # 1. Delete all traces of old word
         for i in range(len(byteword) - 1):
             pair = (byteword[i], byteword[i + 1])
             paircounts[pair] -= currcount
             vocabmap[pair].discard(byteword)
-            
+
         # 2. Construct new word
         construct = []
         i = 0
@@ -83,84 +86,90 @@ def _apply_merge(
                 i += 1
         if i == len(byteword) - 1:
             construct.append(byteword[i])
-            
+
         new_word = tuple(construct)
-        
+
         # 3. Add counts and vocabmap for new word
         for i in range(len(new_word) - 1):
             pair = (new_word[i], new_word[i + 1])
             paircounts[pair] = paircounts.get(pair, 0) + currcount
             vocabmap[pair].add(new_word)
-            
+
         # 4. Update main ledger
         pretokenized_vocab.pop(byteword)
-        pretokenized_vocab[new_word] = pretokenized_vocab.get(new_word, 0) + currcount
-        
+        pretokenized_vocab[new_word] = pretokenized_vocab.get(
+            new_word, 0) + currcount
+
     vocabmap[bestpair] = set()
 
+
 def train_bpe(
-    input_path: str, 
+    input_path: str,
     vocab_size: int,
     special_tokens: list[str]
 ) -> tuple[dict[int, bytes], list[tuple[bytes, bytes]]]:
-  """
-  Trains a Byte Pair Encoding tokenizer
-  """
+    """
+    Trains a Byte Pair Encoding tokenizer
+    """
 
-  num_cores = 8
+    num_cores = 8
 
-  # Open file/corpus
-  with open(input_path, "rb") as f:
-    boundaries = find_chunk_boundaries(f, num_cores, b"<|endoftext|>")
+    # Open file/corpus
+    with open(input_path, "rb") as f:
+        boundaries = find_chunk_boundaries(f, num_cores, b"<|endoftext|>")
 
-  pool_args = []
-  for start, end in zip(boundaries[:-1], boundaries[1:]):
-    pool_args.append((input_path, start, end, special_tokens))
+    pool_args = []
+    for start, end in zip(boundaries[:-1], boundaries[1:]):
+        pool_args.append((input_path, start, end, special_tokens))
 
-  # Initialize vocab and counts
-  with mp.Pool(processes = num_cores) as pool:
-    vocabperchunk = pool.starmap(_get_pretokenized_vocab, pool_args)
+    # Initialize vocab and counts
+    with mp.Pool(processes=num_cores) as pool:
+        vocabperchunk = pool.starmap(_get_pretokenized_vocab, pool_args)
 
-  pretokenized_vocab = {}
-  for minivocab in vocabperchunk:
-    for word, count in minivocab.items():
-       pretokenized_vocab[word] = pretokenized_vocab.get(word, 0) + count
-     
+    pretokenized_vocab = {}
+    for minivocab in vocabperchunk:
+        for word, count in minivocab.items():
+            pretokenized_vocab[word] = pretokenized_vocab.get(word, 0) + count
 
-  paircounts, vocabmap = _counts_and_vocabmap(pretokenized_vocab)
+    paircounts, vocabmap = _counts_and_vocabmap(pretokenized_vocab)
 
-  currsize = 256
-  vocab = {i: bytes([i]) for i in range(currsize)}
-  merges = []
+    currsize = 256
+    vocab = {i: bytes([i]) for i in range(currsize)}
+    merges = []
 
-  # Merge until desired vocab size
-  while currsize < (vocab_size - len(special_tokens)):
-    # no pairs to merge
-    if len(paircounts) == 0:
-      break
-    
-    bestpair = tuple()
-    bestcount = 0
+    # Merge until desired vocab size
+    while currsize < (vocab_size - len(special_tokens)):
+        # no pairs to merge
+        if len(paircounts) == 0:
+            break
 
-    # Prioritize lexicographically greater pair
-    for pair, count in paircounts.items():
-      if count > bestcount:
-        bestpair = pair
-        bestcount = count
-      elif count == bestcount and pair > bestpair:
-        bestpair = pair
-    
-    merges.append(bestpair)
-    vocab[currsize] = bestpair[0] + bestpair[1]
-    currsize += 1
-    paircounts[bestpair] = 0
+        bestpair = tuple()
+        bestcount = 0
 
-    # update the counts and dict
-    _apply_merge(bestpair, pretokenized_vocab, paircounts, vocabmap)
-    
-  # add the special tokens
-  for i in range(len(special_tokens)):
-    vocab[currsize] = special_tokens[i].encode("utf-8")
-    currsize += 1
+        # Prioritize lexicographically greater pair
+        for pair, count in paircounts.items():
+            if count > bestcount:
+                bestpair = pair
+                bestcount = count
+            elif count == bestcount and pair > bestpair:
+                bestpair = pair
 
-  return (vocab, merges)
+        if bestcount == 0:
+            break
+
+        merges.append(bestpair)
+        vocab[currsize] = bestpair[0] + bestpair[1]
+        currsize += 1
+        paircounts[bestpair] = 0
+
+        # update the counts and dict
+        _apply_merge(bestpair, pretokenized_vocab, paircounts, vocabmap)
+        # if currsize % 200 == 0:
+        #   print(f"[{time.time()-t0:.0f}s] merges={currsize} unique_pairs={len(paircounts)} unique_words={len(pretokenized_vocab)}", flush=True)
+
+    # add the special tokens
+    for i in range(len(special_tokens)):
+        vocab[currsize] = special_tokens[i].encode("utf-8")
+        currsize += 1
+
+    return (vocab, merges)
---
+++ cs336_basics/tokenizer.py
@@ -0,0 +1,119 @@
+import pickle
+import regex as re
+from typing import Iterable, Iterator
+
+
+class Tokenizer:
+    def __init__(self, vocab: dict[int, bytes], merges: list[tuple[bytes, bytes]], special_tokens=None):
+        self.vocab = vocab
+        self.merges = merges
+        self.special_tokens = special_tokens or []
+
+        # for encoding
+        self.merges_dict = {pair: rank for rank,
+                            pair in enumerate(self.merges)}
+        self.vocab_dict = {pair: num for num, pair in self.vocab.items()}
+
+    # assumes vocab and merges are saved as pickle
+    @classmethod
+    def from_files(cls, vocab_filepath: str, merges_filepath: str, special_tokens=None):
+        vocab = {}
+        merges = []
+        with open(vocab_filepath) as f:
+            vocab = pickle.load(f)
+
+        with open(merges_filepath) as f:
+            merges = pickle.load(f)
+
+        return cls(vocab, merges, special_tokens)
+
+    def _pretokenize(
+        self,
+        text: str,
+        special_tokens: list[str] = None
+    ) -> list[str]:
+        """
+        Splits text using the BPE regex. If special_tokens are provided,
+        they are isolated and kept intact.
+        """
+        PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+        res = []
+
+        if not special_tokens:
+            for m in re.finditer(PAT, text):
+                res.append(m.group())
+            return res
+
+        specials_pattern = f"({'|'.join([re.escape(token) for token in special_tokens])})"
+        splitted = re.split(specials_pattern, text)
+
+        for spl in splitted:
+            if not spl:
+                continue
+            if spl in special_tokens:
+                res.append(spl)
+            else:
+                for m in re.finditer(PAT, spl):
+                    res.append(m.group())
+
+        return res
+
+    def encode(self, text: str) -> list[int]:
+        regsplit = self._pretokenize(text, self.special_tokens)
+        mdict = self.merges_dict
+        vdict = self.vocab_dict
+
+        res = []
+        encodingdict = {}
+        for t in regsplit:
+            if t in encodingdict:
+                res.extend(encodingdict[t])
+                continue
+
+            currword = tuple(bytes([b]) for b in t.encode("utf-8"))
+            while True:
+                bestpair = ()
+                bestrank = float('inf')
+                for i in range(0, len(currword) - 1):
+                    currpair = (currword[i], currword[i + 1])
+                    if currpair in mdict and mdict[currpair] < bestrank:
+                        bestpair = currpair
+                        bestrank = mdict[currpair]
+
+                if bestrank == float('inf'):
+                    break
+
+                construct = []
+                i = 0
+                while i < (len(currword) - 1):
+                    pair = (currword[i], currword[i + 1])
+                    if pair == bestpair:
+                        construct.append(currword[i] + currword[i + 1])
+                        i += 2
+                    else:
+                        construct.append(currword[i])
+                        i += 1
+                if i == len(currword) - 1:
+                    construct.append(currword[i])
+                currword = tuple(construct)
+
+            encoded = []
+            for b in currword:
+                encoded.append(vdict[b])
+
+            encodingdict[t] = encoded
+            res.extend(encoded)
+        return res
+
+    # lazy
+    def encode_iterable(self, iterable: Iterable[str]) -> Iterator[int]:
+        for chunk in iterable:
+            chunk_ids = self.encode(chunk)
+            yield from chunk_ids
+
+    def decode(self, ids: list[int]) -> str:
+        """
+        decodes a sequence of ids by joining all the byte representation first and then decoding it all at once
+        """
+        total = b"".join([self.vocab[num] for num in ids])
+        return total.decode("utf-8")
